ac159d47a6a5a5cf7f1538fcca45461b725228c1
ridhwan <ridmartrite@gmail.com>
2023-04-22 08:22:12 +0100

    completed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
+
 def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
     res = [[1]]
     for i in range(n -1):
         temp = [0] + res[-1] + [0]
