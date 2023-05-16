3d302961eef3f4931049f1b5fc2a753dbca5e06d
ridhwan <ridmartrite@gmail.com>
2023-05-16 02:39:46 +0100

    adding task again

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,7 +5,7 @@ import re
 
 
 def extract_input(input_line):
-    '''Removes sections of a line of an HTTP request log.
+    '''sections of a line of an HTTP request log.
     '''
     fp = (
         r'\s*(?P<ip>\S+)\s*',
@@ -29,7 +29,7 @@ def extract_input(input_line):
 
 
 def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
+    '''accumulated statistics of the HTTP request log.
     '''
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
@@ -39,13 +39,13 @@ def print_statistics(total_file_size, status_codes_stats):
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
-    ''' metrics  updates from a given HTTP request log.
+    ''' metrics from a given HTTP request log.
 
     Args:
         line (str): The line of input from which to retrieve the metrics.
 
     Returns:
-        int: new total file size.
+        int: The new total file size.
     '''
     line_info = extract_input(line)
     status_code = line_info.get('status_code', '0')
@@ -55,7 +55,7 @@ def update_metrics(line, total_file_size, status_codes_stats):
 
 
 def run():
-    '''log parser starts.
+    '''Starts the log parser.
     '''
     line_num = 0
     total_file_size = 0
